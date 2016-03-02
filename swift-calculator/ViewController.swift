cfab92be8780f3b69c3c6d4a81898eccbf658a4f
Mark Kipyegon <kipyegonmark@protonmail.ch>
2016-03-02 12:15:42 +0300

    Added operation functions

--- swift-calculator/ViewController.swift
+++ swift-calculator/ViewController.swift
@@ -24,7 +24,51 @@ class ViewController: UIViewController {
             userInput = true
         }
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userInput {
+            enter()
+        }
+        switch operation {
+            case "＊": performOperation { $0 * $1}
+            case "∕" : performOperation { $1 / $0}
+            case "+" : performOperation { $0 + $1}
+            case "-" : performOperation { $1 - $0}
+            case "√" : performSqrt { sqrt($0)}
+        default : break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performSqrt(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack: Array<Double> = Array<Double>()
+    
     @IBAction func enter() {
         userInput = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userInput = false
+        }
     }
 }
